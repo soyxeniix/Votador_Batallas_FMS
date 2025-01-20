9f8dcffb9204f00bbab813c957717c29fe45bed0
soyxeniix <juan.gutierrez.florez@correounivalle.edu.co>
2025-01-20 15:10:50 -0500

    Informacion de contacto añadida

---
+++ VotadorBatallas.py
@@ -0,0 +1,146 @@
+
+#Nombres
+
+print("Nombres de los MC")
+
+nombre_MC1 = input("Ingrese el nombre del Primer Mc: ")
+nombre_MC2 = input("Ingrese el nombre del Segundo Mc: ")
+
+# Random mode
+
+print("Randon Mode")
+
+primer_patronR_MC1 = float(input(f"Puntaje primer patron {nombre_MC1}: "))
+segundo_patronR_MC1 = float(input(f"Puntaje segundo patron {nombre_MC1}: "))
+
+primer_patronR_MC2 = float(input(f"Puntaje primer patron {nombre_MC2}: "))
+segundo_patronR_MC2 = float(input(f"Puntaje segundo patron {nombre_MC2}: "))
+
+tercer_patronR_MC1 = float(input(f"Puntaje tercer patron {nombre_MC1}: "))
+cuarto_patronR_MC1 = float(input(f"Puntaje cuarto patron {nombre_MC1}: "))
+
+tercer_patronR_MC2 = float(input(f"Puntaje tercer patron {nombre_MC2}: "))
+cuarto_patronR_MC2 = float(input(f"Puntaje cuarto patron {nombre_MC2}: "))
+
+quinto_patronR_MC1 = float(input(f"Puntaje quinto patron {nombre_MC1}: "))
+sexto_patronR_MC1 = float(input(f"Puntaje sexto patron {nombre_MC1}: "))
+
+quinto_patronR_MC2 = float(input(f"Puntaje quinto patron {nombre_MC2}: "))
+sexto_patronR_MC2 = float(input(f"Puntaje sexto patron {nombre_MC2}: "))
+
+septimo_patronR_MC1 = float(input(f"Puntaje septimo patron {nombre_MC1}: "))
+octavo_patronR_MC1 = float(input(f"Puntaje octavo patron {nombre_MC1}: "))
+
+septimo_patronR_MC2 = float(input(f"Puntaje septimo patron {nombre_MC2}: "))
+octavo_patronR_MC2 = float(input(f"Puntaje octavo patron {nombre_MC2}: "))
+
+# Minutos libre
+
+print("MINUTOS LIBRES\n  Primer Minuto {nombre_MC2}")
+
+primer_patronM_MC2 = float(input(f"Puntaje primer patron {nombre_MC2}: "))
+segundo_patronM_MC2 = float(input(f"Puntaje segundo patron {nombre_MC2}: "))
+tercer_patronM_MC2 = float(input(f"Puntaje tercer patron {nombre_MC2}: "))
+cuarto_patronM_MC2 = float(input(f"Puntaje cuarto patron {nombre_MC2}: "))
+quinto_patronM_MC2 = float(input(f"Puntaje quinto patron {nombre_MC2}: "))
+sexto_patronM_MC2 = float(input(f"Puntaje sexto patron {nombre_MC2}: "))
+
+print(f"Primer Minuto {nombre_MC1}")
+
+primer_patronM_MC1 = float(input(f"Puntaje primer patron {nombre_MC1}: "))
+segundo_patronM_MC1 = float(input(f"Puntaje segundo patron {nombre_MC1}: "))
+tercer_patronM_MC1 = float(input(f"Puntaje tercer patron {nombre_MC1}: "))
+cuarto_patronM_MC1 = float(input(f"Puntaje cuarto patron {nombre_MC1}: "))
+quinto_patronM_MC1 = float(input(f"Puntaje quinto patron {nombre_MC1}: "))
+sexto_patronM_MC1 = float(input(f"Puntaje sexto patron {nombre_MC1}: "))
+
+print(f"Segundo Minuto {nombre_MC1}")
+
+primer_patronM2_MC1 = float(input(f"Puntaje primer patron {nombre_MC1}: "))
+segundo_patronM2_MC1 = float(input(f"Puntaje segundo patron {nombre_MC1}: "))
+tercer_patronM2_MC1 = float(input(f"Puntaje tercer patron {nombre_MC1}: "))
+cuarto_patronM2_MC1 = float(input(f"Puntaje cuarto patron {nombre_MC1}: "))
+quinto_patronM2_MC1 = float(input(f"Puntaje quinto patron {nombre_MC1}: "))
+sexto_patronM2_MC1 = float(input(f"Puntaje sexto patron {nombre_MC1}: "))
+
+print(f"Segundo Minuto {nombre_MC2}")
+
+primer_patronM2_MC2 = float(input(f"Puntaje primer patron {nombre_MC2}: "))
+segundo_patronM2_MC2 = float(input(f"Puntaje segundo patron {nombre_MC2}: "))
+tercer_patronM2_MC2 = float(input(f"Puntaje tercer patron {nombre_MC2}: "))
+cuarto_patronM2_MC2 = float(input(f"Puntaje cuarto patron {nombre_MC2}: "))
+quinto_patronM2_MC2 = float(input(f"Puntaje quinto patron {nombre_MC2}: "))
+sexto_patronM2_MC2 = float(input(f"Puntaje sexto patron {nombre_MC2}: "))
+
+# Acapela
+
+print("Acapella")
+
+primer_entrada_acapella_MC2 = float(input(f"Primer entrada acapella {nombre_MC2}: "))
+primer_entrada_acapella_MC1 = float(input(f"Primer entrada acapella {nombre_MC1}: "))
+segunda_entrada_acapella_MC2 = float(input(f"Segunda entrada acapella {nombre_MC2}: "))
+segunda_entrada_acapella_MC1 = float(input(f"Segunda entrada acapella {nombre_MC1}: "))
+tercer_entrada_acapella_MC2 = float(input(f"Tercera entrada acapella {nombre_MC2}: "))
+tercer_entrada_acapella_MC1 = float(input(f"Tercera entrada acapella {nombre_MC1}: "))
+
+# Round 4x4
+print("4x4")
+
+primer_patron_4x4_MC2 = float(input(f"Primer patrón {nombre_MC2}: "))
+primer_patron_4x4_MC1 = float(input(f"Primer patrón {nombre_MC1}: "))
+segundo_patron_4x4_MC2 = float(input(f"Segundo patrón {nombre_MC2}: "))
+segundo_patron_4x4_MC1 = float(input(f"Segundo patrón {nombre_MC1}: "))
+tercer_patron_4x4_MC2 = float(input(f"Tercer patrón {nombre_MC2}: "))
+tercer_patron_4x4_MC1 = float(input(f"Tercer patrón {nombre_MC1}: "))
+cuarto_patron_4x4_MC2 = float(input(f"Cuarto patrón {nombre_MC2}: "))
+cuarto_patron_4x4_MC1 = float(input(f"Cuarto patrón {nombre_MC1}: "))
+quinto_patron_4x4_MC2 = float(input(f"Quinto patrón {nombre_MC2}: "))
+quinto_patron_4x4_MC1 = float(input(f"Quinto patrón {nombre_MC1}: "))
+sexto_patron_4x4_MC2 = float(input(f"Sexto patrón {nombre_MC2}: "))
+sexto_patron_4x4_MC1 = float(input(f"Sexto patrón {nombre_MC1}: "))
+septimo_patron_4x4_MC2 = float(input(f"Séptimo patrón {nombre_MC2}: "))
+septimo_patron_4x4_MC1 = float(input(f"Séptimo patrón {nombre_MC1}: "))
+octavo_patron_4x4_MC2 = float(input(f"Octavo patrón {nombre_MC2}: "))
+octavo_patron_4x4_MC1 = float(input(f"Octavo patrón {nombre_MC1}: "))
+
+#Resultados
+
+print("RESULTADOS")
+
+puntaje_randonmode_MC1 = float(primer_patronR_MC1+segundo_patronR_MC1+tercer_patronR_MC1+
+                                cuarto_patronR_MC1+quinto_patronR_MC1+sexto_patronR_MC1+septimo_patronR_MC1+octavo_patronR_MC1)
+
+puntaje_minutos_MC1 = float(primer_patronM_MC1+segundo_patronM_MC1+tercer_patronM_MC1+cuarto_patronM_MC1+quinto_patronM_MC1+sexto_patronM_MC1+
+                            primer_patronM2_MC1+segundo_patronM2_MC1+tercer_patronM2_MC1+cuarto_patronM2_MC1+quinto_patronM2_MC1+sexto_patronM2_MC1)
+
+puntaje_deluxe_MC1 = float(primer_entrada_acapella_MC1+segunda_entrada_acapella_MC1+tercer_entrada_acapella_MC1+
+                            primer_patron_4x4_MC1+segundo_patron_4x4_MC1+tercer_patron_4x4_MC1+cuarto_patron_4x4_MC1+quinto_patron_4x4_MC1+sexto_patron_4x4_MC1
+                            +septimo_patron_4x4_MC1+octavo_patron_4x4_MC1)                  
+
+puntaje_total_MC1 = float(puntaje_randonmode_MC1+puntaje_minutos_MC1+puntaje_deluxe_MC1)
+          
+puntaje_randonmode_MC2 = float(primer_patronR_MC2+segundo_patronR_MC2+tercer_patronR_MC2+
+                                cuarto_patronR_MC2+quinto_patronR_MC2+sexto_patronR_MC2+septimo_patronR_MC2+octavo_patronR_MC2)
+
+puntaje_minutos_MC2 = float(primer_patronM_MC2+segundo_patronM_MC2+tercer_patronM_MC2+cuarto_patronM_MC2+quinto_patronM_MC2+sexto_patronM_MC2+
+                            primer_patronM2_MC2+segundo_patronM2_MC2+tercer_patronM2_MC2+cuarto_patronM2_MC2+quinto_patronM2_MC2+sexto_patronM2_MC2)
+                            
+puntaje_deluxe_MC2 = float(primer_entrada_acapella_MC2+segunda_entrada_acapella_MC2+tercer_entrada_acapella_MC2+
+                            primer_patron_4x4_MC2+segundo_patron_4x4_MC2+tercer_patron_4x4_MC1+cuarto_patron_4x4_MC2+quinto_patron_4x4_MC2+sexto_patron_4x4_MC2
+                            +septimo_patron_4x4_MC2+octavo_patron_4x4_MC2)
+
+puntaje_total_MC2 = float(puntaje_randonmode_MC2+puntaje_minutos_MC2+puntaje_deluxe_MC2)
+
+
+print(f"Puntaje Random mode {nombre_MC1} = {puntaje_randonmode_MC1}")
+print(f"Puntaje Random mode {nombre_MC2} = {puntaje_randonmode_MC2}")
+
+print(f"Puntaje Minutos {nombre_MC1} = {puntaje_minutos_MC1}")
+print(f"Puntaje Minutos {nombre_MC2} = {puntaje_minutos_MC2}")
+
+print(f"Puntaje Deluxe {nombre_MC1} = {puntaje_deluxe_MC1}")
+print(f"Puntaje Deluxe {nombre_MC2} = {puntaje_deluxe_MC2}")
+
+print(f"Puntaje total Batalla {nombre_MC1} = {puntaje_total_MC1}")
+print(f"Puntaje total Batalla {nombre_MC2} = {puntaje_total_MC2}")
+
